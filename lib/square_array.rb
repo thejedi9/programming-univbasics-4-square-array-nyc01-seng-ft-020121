f2b63aefe4607bc51f399a003caa244efd274e64
Jede Brahma <brahmajede@gmail.com>
2021-01-27 19:42:00 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,20 +1,20 @@
 def square_array(numbers)
-  numbers = [1,2,3]
+  numbers = ([1,2,3])
   counter = 0
   sqrd_numbers=[]
   while numbers[counter] do
-    sqrd_numbers<< (numbers[counter]**2)
+    sqrd_numbers<< (numbers[counter])**2
     counter += 1
   end
   puts sqrd_numbers
 end
   
 def new_square_array(new_numbers)
-  new_numbers = [9,10,16,25]
+  new_numbers = ([9,10,16,25])
   counter = 0
   new_sqr_numbers=[]
   while new_numbers[counter] do
-    new_sqr_numbers<< (new_numbers[counter]**2)
+    new_sqr_numbers<< (new_numbers[counter])**2
     counter += 1
   end
   new_sqr_numbers
