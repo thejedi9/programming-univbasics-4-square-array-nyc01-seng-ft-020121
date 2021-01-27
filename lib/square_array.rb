d8a96e16df5b309fe26cd9777fcf75983010c347
Jede Brahma <brahmajede@gmail.com>
2021-01-27 17:05:51 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -5,15 +5,16 @@ def square_array(numbers)
   while numbers[counter] do
     sqrd_numbers << numbers[counter]*numbers[counter]
     counter += 1
-    end
+   
     sqrd_numbers
-
+   end
   new_numbers = [9,10,16,25]
   counter = 0
   new_sqr_numbers=[]
   while new_numbers[counter] do
     new_sqr_numbers << new_numbers[counter]*new_numbers[counter]
     counter += 1
-    end
+  
     new_sqr_numbers
+    end
   end
